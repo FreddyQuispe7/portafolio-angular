23345fc15675467115f220952996c623a5000402
Freddy QuispeInfosil <fquispe@smartsolutions.com.pe>
2020-12-30 18:02:11 -0500

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { Equipo } from '../interfaces/equipo.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,18 +10,29 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: Equipo[] = [];
+  cargada2 = false;
 
-  constructor( private http: HttpClient) {
+  constructor( private http: HttpClient ) {
 
-    //console.log('Servicio de Info pagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo() {
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
-        console.log(resp);
       });
-  
   }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-hmtl-7f057-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe( (resp: Equipo[]) => {
+        this.equipo = resp;
+      });
+  }
+
 }
